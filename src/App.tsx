d601b53dec45c75fa374e2b231078a6808c19d13
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-04-04 23:59:28 +0530

    accordian done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import Header from "./components/Header";
 import {Infinitescrollbrand} from "./components/infinitescrollbrand";
-import { SvgComponent } from "./components/image";
-import { SvgComponent2 } from "./components/image2";
-import { SvgComponent3 } from "./components/image3";
-import { SvgComponent5 } from "./components/image5";
-import { SvgComponent4 } from "./components/image4";
-import { SvgComponent6 } from "./components/image6";
 import Blackbutton from "./components/Blackbutton";
 import Hero from "./components/Hero";
 import CustomizedAccordions from "./components/Accordian";
@@ -19,12 +13,12 @@ function App() {
       <Infinitescrollbrand/>
       <Para/>
 <div className="w-full flex flex-row justify-around h-auto">
-<div className="sm:w-5/12 w-11/12">
+<div className="sm:w-full w-full mt-40 mx-20">
 <CustomizedAccordions/>
 </div>
-<div className="w-5/12  sm:block hidden ">
-<SvgComponent6 />
-</div>
+{/* <div className="w-5/12  sm:block hidden ">
+<SvgComponent7 />
+</div> */}
 </div>
     </div>
   );
--- src/components/Accordian.tsx
+++ src/components/Accordian.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import { SvgComponent } from './image';
+import { SvgComponent2 } from './image2';
+import { SvgComponent3 } from './image3';
 import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
 import MuiAccordionSummary, {
   AccordionSummaryProps,
 } from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import { SvgComponent4 } from './image4';
+import { SvgComponent5 } from './image5';
+import { SvgComponent6 } from './image6';
+import { SvgComponent7 } from './image7';
 
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
-    borderBottom: 0,
-  },
+))(({}) => ({
+  
   '&::before': {
     display: 'none',
   },
@@ -22,14 +25,13 @@ const Accordion = styled((props: AccordionProps) => (
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    // expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
     {...props}
   />
 ))(({ theme }) => ({
   backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .03)',
+  'rgb(22,22,24)'
+   ,
   flexDirection: 'row-reverse',
   '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
     transform: 'rotate(90deg)',
@@ -41,7 +43,7 @@ const AccordionSummary = styled((props: AccordionSummaryProps) => (
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  // borderTop: '1px solid rgba(0, 0, 0, .125)',
 }));
 
 export default function CustomizedAccordions() {
@@ -53,46 +55,145 @@ export default function CustomizedAccordions() {
     };
 
   return (
-    <div >
+    <div className="flex flex-row">
+      <div className='w-6/12' >
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Collapsible Group Item #1</Typography>
+        <AccordionSummary  aria-controls="panel1d-content" id="panel1d-header">
+          <Typography className=''>
+            <div className="font-bold text-2xl text-white">
+            Contract Lifecycle Management
+            </div>
+          </Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails className='bg-[rgb(22,22,24)]'>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+            <div className="flex flex-row">
+              <div className="text-white w-6/12 text-xl">Easily create, review, edit, redline, approve, eSign and store contracts in one intuitive workflow. Collaborate with your team and negotiate with counterparties. Powered by AI.</div>
+              {/* <div className="w-6/12">
+                <SvgComponent/>
+              </div> */}
+            </div>
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Collapsible Group Item #2</Typography>
+        <Typography className=''>
+            <div className="font-bold text-2xl text-white">
+            Contract Lifecycle Management
+            </div>
+          </Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails className='bg-[rgb(22,22,24)]'>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+            <div className="flex flex-row">
+              <div className="text-white w-6/12 text-xl">Easily create, review, edit, redline, approve, eSign and store contracts in one intuitive workflow. Collaborate with your team and negotiate with counterparties. Powered by AI.</div>
+              {/* <div className="w-6/12">
+                <SvgComponent/>
+              </div> */}
+            </div>
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
         <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>Collapsible Group Item #3</Typography>
+        <Typography className=''>
+            <div className="font-bold text-2xl text-white">
+            Contract Lifecycle Management
+            </div>
+          </Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails className='bg-[rgb(22,22,24)]'>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+            <div className="flex flex-row">
+              <div className="text-white w-6/12 text-xl">Easily create, review, edit, redline, approve, eSign and store contracts in one intuitive workflow. Collaborate with your team and negotiate with counterparties. Powered by AI.</div>
+              {/* <div className="w-6/12">
+                <SvgComponent/>
+              </div> */}
+            </div>
           </Typography>
         </AccordionDetails>
       </Accordion>
+      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
+        <Typography className=''>
+            <div className="font-bold text-2xl text-white">
+            Contract Lifecycle Management
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails className='bg-[rgb(22,22,24)]'>
+          <Typography>
+            <div className="flex flex-row">
+              <div className="text-white w-6/12 text-xl">Easily create, review, edit, redline, approve, eSign and store contracts in one intuitive workflow. Collaborate with your team and negotiate with counterparties. Powered by AI.</div>
+              {/* <div className="w-6/12">
+                <SvgComponent/>
+              </div> */}
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+        <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">
+        <Typography className=''>
+            <div className="font-bold text-2xl text-white">
+            Contract Lifecycle Management
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails className='bg-[rgb(22,22,24)]'>
+          <Typography>
+            <div className="flex flex-row">
+              <div className="text-white w-6/12 text-xl">Easily create, review, edit, redline, approve, eSign and store contracts in one intuitive workflow. Collaborate with your team and negotiate with counterparties. Powered by AI.</div>
+              {/* <div className="w-6/12">
+                <SvgComponent/>
+              </div> */}
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+        <AccordionSummary aria-controls="panel6d-content" id="panel6d-header">
+        <Typography className=''>
+            <div className="font-bold text-2xl text-white">
+            Contract Lifecycle Management
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails className='bg-[rgb(22,22,24)]'>
+          <Typography>
+            <div className="flex flex-row">
+              <div className="text-white w-6/12 text-xl">Easily create, review, edit, redline, approve, eSign and store contracts in one intuitive workflow. Collaborate with your team and negotiate with counterparties. Powered by AI.</div>
+              {/* <div className="w-6/12">
+                <SvgComponent/>
+              </div> */}
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
+        <AccordionSummary aria-controls="panel7d-content" id="panel7d-header">
+        <Typography className=''>
+            <div className="font-bold text-2xl text-white">
+            Contract Lifecycle Management
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails className='bg-[rgb(22,22,24)]'>
+          <Typography>
+            <div className="flex flex-row">
+              <div className="text-white w-6/12 text-xl">Easily create, review, edit, redline, approve, eSign and store contracts in one intuitive workflow. Collaborate with your team and negotiate with counterparties. Powered by AI.</div>
+              {/* <div className="w-6/12">
+                <SvgComponent/>
+              </div> */}
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+    <div className="w-6/12 ">
+{expanded==='panel1'?<SvgComponent/>:expanded==='panel2'?<SvgComponent2/>:expanded==='panel3'?<SvgComponent3/>:expanded==='panel4'?<SvgComponent4/>:expanded==='panel5'?<SvgComponent5/>:expanded==='panel6'?<SvgComponent6/>:<SvgComponent7/>}
+    </div>
     </div>
   );
 }
