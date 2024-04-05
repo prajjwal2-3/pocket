86181ed11429796a008e02943f86edbfbd1364d4
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-04-05 23:09:30 +0530

    made it responsive

--- src/components/Moun.tsx
+++ src/components/Moun.tsx
@@ -6,10 +6,10 @@ const Moun = () => {
       <div className="bg-gradient-to-b  from-[rgb(22,22,24)] to-transparent absolute  w-full h-[30rem] z-10"></div>
       <img className=" object-cover object-center w-full h-[30rem] absolute"  src="https://framerusercontent.com/images/nxT02XSEg6U0ivCS5ojfouAZTk.png?scale-down-to=2048" alt="" />
        <div className="absolute z-20 w-full  flex flex-col items-center  text-center">
-        <div className="text-yellow-400 w-7/12 mt-16 font-serif text-[4rem] font-bold ">
+        <div className="text-yellow-400 sm:w-7/12 w-11/12 sm:mt-16 mt-28 font-serif sm:text-[4rem] text-[1.5rem] font-bold ">
         Siloed legal solutions slow you down and create risks
         </div>
-        <div className="text-center text-white/80 my-5 font-semibold sm:text-xl w-6/12">
+        <div className="text-center text-white/80 my-5 font-semibold sm:text-xl sm:w-6/12 w-11/12">
         When you use different solutions for your legal work you create organizational drag and introduce unnecessary risk with lack of visibility.
         </div>
         <div className="">
